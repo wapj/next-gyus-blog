9f660adc1e57828803527cb24cc7275885a94a6a
andy.sg <andy.sg@kakaoent.com>
2023-07-28 10:34:16 +0900

    태그가 있으면 태그도 보여주기

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import './globals.css'
 import Link from 'next/link'
 import React from "react";
-import {formatDate} from "@/libs/utils";
 import RightArrow from "@/components/icons/RightArrow";
 import getAllFilesFrontMatter from "@/libs/mdx";
 
@@ -13,14 +12,14 @@ async function getData() {
   }
 }
 
-export default async function Home(props : any) {
+export default async function Home() {
   const {posts} = await getData();
   return (
     <>
       <main className={"max-w-screen-lg px-4 py-3 mx-auto"}>
         🚧공사중...
         <div className={"mt-10 space-y-16"}>
-          {posts.map(({title, slug, date, summary}: any) => (
+          {posts.map(({title, slug, date, summary, tags}: any) => (
             <article key={slug} className="relative group">
               <div className="relative">
                 <h3 className="text-base font-semibold tracking-tight text-slate-900 dark:text-slate-200 pt-8 lg:pt-0">
@@ -31,6 +30,12 @@ export default async function Home(props : any) {
                   dangerouslySetInnerHTML={{ __html: summary }}
                 />
                 {date.split("T")[0]}
+                <br />
+                {tags && tags.map((tag: string) => (
+                  <div className="rounded bg-gray-200 pl-1 pr-1 inline-block mr-2">
+                    <span className="text-xs text-gray-800 font-bold">{tag}</span>
+                  </div>
+                  ))}
               </div>
               <Link href={`/blog/${slug}`} className="flex items-center text-sm text-sky-500 font-medium">
                 <span className="relative">
